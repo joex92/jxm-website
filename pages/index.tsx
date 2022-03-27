3388453faf5754426eb3c8c8f3c1f005638f35a8
Joe X <jamedinao@gmail.com>
2022-03-27 10:25:11 -0500

    Social Media

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,69 +2,74 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 
+const socialmedia = [
+  {
+    name: 'Soundcloud',
+    url: 'https://soundcloud.com/joex92',
+    desc: ''
+  },
+  {
+    name: 'YouTube',
+    url: 'https://youtube.com/c/joex92',
+    desc: ''
+  },
+  {
+    name: 'Facebook',
+    url: 'https://facebook.com/JoeX92',
+    desc: ''
+  },
+  {
+    name: 'Instagram',
+    url: 'https://instagram.com/J03X92',
+    desc: ''
+  },
+  {
+    name: 'Twitter',
+    url: 'https://twitter.com/joex92',
+    desc: ''
+  },
+  {
+    name: 'Tumblr',
+    url: 'https://joex92.tumblr.com/',
+    desc: ''
+  }
+]
+
 const Home: NextPage = () => {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
       <Head>
-        <title>Create Next App</title>
+        <title>JxM!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
+      <header>
+        <div>
+          <ul>
+            :v
+          </ul>
+        </div>
+      </header>
       <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
         <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
+          JxM!
         </h1>
 
         <p className="mt-3 text-2xl">
-          Get started by editing{' '}
           <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
+            Coding & Music Production
           </code>
         </p>
 
         <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
+          {socialmedia.map((v,i)=><a
+            href={v.url}
+            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-red-600 focus:text-red-600"
           >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
+            <h3 className="text-2xl font-bold">{v.name}</h3>
             <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
+              {v.desc}
             </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          </a>)}
         </div>
       </main>
 
