b8c3cba98cfa6ca932fb91a7700659671a80072b
Joe X <jamedinao@gmail.com>
2022-03-28 11:50:58 -0500

    Added Social Media links & modules for future use

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,47 +6,44 @@ const socialmedia = [
   {
     name: 'Soundcloud',
     url: 'https://soundcloud.com/joex92',
-    desc: ''
   },
   {
     name: 'YouTube',
     url: 'https://youtube.com/c/joex92',
-    desc: ''
   },
   {
     name: 'Facebook',
     url: 'https://facebook.com/JoeX92',
-    desc: ''
   },
   {
     name: 'Instagram',
     url: 'https://instagram.com/J03X92',
-    desc: ''
   },
   {
     name: 'Twitter',
     url: 'https://twitter.com/joex92',
-    desc: ''
   },
   {
     name: 'Tumblr',
     url: 'https://joex92.tumblr.com/',
-    desc: ''
   }
 ]
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="flex min-h-screen w-full flex-col items-center justify-center p-0">
       <Head>
         <title>JxM!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <header>
-        <div>
-          <ul>
-            :v
-          </ul>
+        <div className="mt-0 flex flex-row w-full flex-nowrap items-center justify-around sm:w-full grid-cols-1">
+          {socialmedia.map((v,i)=><a
+            href={v.url}
+            className="mt-0 w-auto rounded-none border border-t-0 p-3 text-center hover:text-red-600 focus:text-red-600"
+          >
+            <h4 className="text-sm font-bold">{v.name}</h4>
+          </a>)}
         </div>
       </header>
       <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
@@ -60,17 +57,6 @@ const Home: NextPage = () => {
           </code>
         </p>
 
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          {socialmedia.map((v,i)=><a
-            href={v.url}
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-red-600 focus:text-red-600"
-          >
-            <h3 className="text-2xl font-bold">{v.name}</h3>
-            <p className="mt-4 text-xl">
-              {v.desc}
-            </p>
-          </a>)}
-        </div>
       </main>
 
       <footer className="flex h-24 w-full items-center justify-center border-t">
