fb68d6c5ae35222937273b20d3054daf2f9829d1
Joe X <jamedinao@gmail.com>
2022-04-21 16:09:04 -0500

    error?

--- pages/index.tsx
+++ pages/index.tsx
@@ -43,8 +43,8 @@ const fetch2b2t = async() => {
     if (!res.ok) {
       const error = new Error('An error occurred while fetching the data.')
       // Attach extra info to the error object.
-      error.info = await res.json()
-      error.status = res.status
+      // error.info = await res.json()
+      // error.status = res.status
       throw error
     }
   
