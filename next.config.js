91d13caba4061d43aa272a4ecae0d0720d6c8971
Joe X <jamedinao@gmail.com>
2022-04-21 16:15:14 -0500

    Access-Control-Allow-Origin to mcAPI

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: "/_next/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Origin", value: "https://mcapi.xdefcon.com" },
+        ],
+      },
+    ]
+  },
 }
