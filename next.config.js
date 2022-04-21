0881661be8b32df31d7f99e35259fe355110d274
Joe X <jamedinao@gmail.com>
2022-04-21 16:19:35 -0500

    ACA

--- next.config.js
+++ next.config.js
@@ -4,11 +4,15 @@ module.exports = {
   async headers() {
     return [
       {
-        source: "/_next/:path*",
+        // matching all routes
+        source: "/:path*",
         headers: [
-          { key: "Access-Control-Allow-Origin", value: "https://mcapi.xdefcon.com" },
-        ],
-      },
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ]
+      }
     ]
-  },
+  }
 }
