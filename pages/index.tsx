e88e68d2e20b64155ea19562fbec5af76d662906
Joe X! <jamedinao@gmail.com>
2023-09-14 13:26:52 -0500

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -73,7 +73,7 @@ const Home: NextPage = () => {
       </header>
       <main className="flex w-full flex-1 flex-col items-center justify-center text-center">
         <h1 className="text-6xl font-bold">
-          <img className="w-fit" src="./JXMO-logo.png" />
+          <a href="https://joex92.alwaysdata.net/" title="Portfolio Website"><img className="w-fit" src="./JXMO-logo.png" /></a>
         </h1>
 
         <p className="mt-3 text-2xl">
